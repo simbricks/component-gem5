cd04b485a0a180bdcee1ffb6f0b7398b67061b55
Jakob Görgen <jakob@simbricks.io>
2026-07-29 13:10:29 +0200

    gem5_sim_py: fix kernel lookup

--- gem5_sim_py/simbricks/components/gem5/simulation/gem5.py
+++ gem5_sim_py/simbricks/components/gem5/simulation/gem5.py
@@ -152,10 +152,10 @@ class Gem5Sim(sim_host.HostSim):
         if self.kernel_path is not None:
             cmd += f"--kernel {inst.env.work_dir_or_abs(self.kernel_path, True)} "
         else:
-            distro_disk = self._disk_images[0][0]
+            distro_disk = self._disk_images[host_spec][0][0]
             if isinstance(distro_disk, disk_images.DistroDiskImage):
                 imp = f"global_input/images/{distro_disk.name}/boot/vmlinux"
-                cmd += f"-kernel {inst.env.work_dir_or_abs(imp, True)}"
+                cmd += f"-kernel {inst.env.work_dir_or_abs(imp, True)} "
             else:
                 raise RuntimeError("Neither a distro disk image nor a kernel path were specified")
         
